a3a452fd8f698df3415927db230df0510b3abd66
zserfvgy156 <zserfvgy115566@gmail.com>
2022-09-12 09:58:54 +0800

    first commit

---
+++ UnitTest/UnitTest/UnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestApp.swift
+//  UnitTest
+//
+//  Created by mike on 2022/9/12.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
